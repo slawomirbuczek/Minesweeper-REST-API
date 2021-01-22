c55332aafa90002bd377f1f3f02fb1981415d84c
Slawomir <slawekbuczek210@gmail.com>
2021-01-22 21:22:53 +0100

    Changed java runtime version to 1.8

---
+++ src/test/java/minesweeper_ranking/MinesweeperLevelEasyApplicationTests.java
@@ -0,0 +1,13 @@
+package minesweeper_ranking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MinesweeperLevelEasyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
